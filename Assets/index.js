125459bcf884a5771d57f712ba25d128f0bde50a
AntYrbis <115887364+AntYrbis@users.noreply.github.com>
2023-06-23 17:54:46 +0200

    Main set up

---
+++ Assets/index.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    let newTheme = 'light';
+    if (matchMedia('(prefers-color-scheme: dark)').matches) {
+        newTheme = 'dark';
+    }
+    const root = document.documentElement;
+    root.className = newTheme;
+};
+
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
